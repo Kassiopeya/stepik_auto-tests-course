a1b6765ceee65e8d0979c5aa0925fdb03f8c0e5e
Kassiopeya <plesovskih.olga@yandex.ru>
2021-04-27 00:39:42 +0300

    задание с assert с заданным условием

---
+++ 3_2_8_assertWithConditions.py
@@ -0,0 +1,5 @@
+def test_input_text(expected_result, actual_result):
+    assert 'expected_result' == 'actual_result', f"expected{expected_result}, got {actual_result}"
+    # можно еще так, но считается устаревшим методом
+    # assert expected_result == actual_result, "expected {}, got {}".format(expected_result, actual_result)
+    # ссылка на задание https://stepik.org/lesson/36285/step/8?thread=solutions&unit=162401
