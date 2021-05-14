bbd53bda8e39ac43d5899d6e3930793fe5dba99e
Kassiopeya <plesovskih.olga@yandex.ru>
2021-05-15 00:43:19 +0300

    ещё одно задание с assert с заданным условием

---
+++ 3_2_9_assertWithConditions2.py
@@ -0,0 +1,4 @@
+def test_substring(full_string, substring):
+    def test_substring(full_string, substring):
+        assert substring in full_string, f"expected '{substring}' to be substring of '{full_string}'"
+    # ссылка на задание https://stepik.org/lesson/36285/step/9?unit=162401
